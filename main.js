6f11d5eb4dae35e67bb931a352acb4365d30d36a
Nguyen Hieu <hieu.nguyen@digitalunicorn.fr>
2022-12-29 22:43:25 +0700

    init code

---
+++ main.js
@@ -0,0 +1,56 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require("electron-updater");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadFile("index.html");
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+  mainWindow.once("ready-to-show", () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
+}
+
+app.on("ready", () => {
+  createWindow();
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+autoUpdater.on("update-available", () => {
+  mainWindow.webContents.send("update_available");
+});
+autoUpdater.on("update-downloaded", () => {
+  mainWindow.webContents.send("update_downloaded");
+});
+
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
+});
+
+ipcMain.on("restart_app", () => {
+  autoUpdater.quitAndInstall();
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
